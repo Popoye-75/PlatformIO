a94d18f1a73c8717bf23923a4a117e4aa5a62d76
Popoye-75 <kasifmohd818@gmail.com>
2026-06-20 00:27:27 +0530

    Updated code

--- Drone/Transmitter/src/Main.cpp
+++ Drone/Transmitter/src/Main.cpp
@@ -1,1 +1,27 @@
+#include <Arduino.h>
+#include <SPI.h>
+#include <RF24.h>
 
+RF24 radio(9, 10); // CE, CSN
+
+void setup()
+{
+    Serial.begin(9600);
+
+    Serial.println("Checking NRF24L01...");
+
+    if (radio.begin())
+    {
+        Serial.println("Transmission is Start ");
+
+        radio.printDetails(); // Module information print karega
+    }
+    else
+    {
+        Serial.println("NRF24L01 NOT Found!");
+    }
+}
+
+void loop()
+{
+}
